18abb2be309618bbfe27a8d7a4145c02964701a0
SamdeVega <s.a.devega.garcia@gmail.com>
2019-07-11 17:31:11 +0100

    Add HomeController and Home view

--- src/main/kotlin/es/leanmind/springreactboilerplate/HomeController.kt
+++ src/main/kotlin/es/leanmind/springreactboilerplate/HomeController.kt
@@ -1,4 +1,12 @@
 package es.leanmind.springreactboilerplate
 
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
 class HomeController {
+    @GetMapping("/home")
+    fun index(): String {
+        return "Home"
+    }
 }
