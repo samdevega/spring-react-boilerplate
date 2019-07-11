a1ff32ecf99b818f4f9d11ee6c9ef0043a98859a
SamdeVega <s.a.devega.garcia@gmail.com>
2019-07-11 09:29:38 +0100

    Initial commit

---
+++ src/main/kotlin/es/leanmind/springreactboilerplate/SpringReactBoilerplateApplication.kt
@@ -0,0 +1,11 @@
+package es.leanmind.springreactboilerplate
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringReactBoilerplateApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringReactBoilerplateApplication>(*args)
+}
---
+++ src/test/kotlin/es/leanmind/springreactboilerplate/SpringReactBoilerplateApplicationTests.kt
@@ -0,0 +1,13 @@
+package es.leanmind.springreactboilerplate
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringReactBoilerplateApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
